08ef414b8da3b23728906d0eeea195cb7658e1c9
hanumantharaovaddi <hanumanthurao9381@gmail.com>
2025-09-03 10:56:01 +0530

    toggle bug fixed

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(true); // default dark
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
+
+    if (storedTheme === "light") {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    } else {
+      // default to dark theme
       setIsDarkMode(true);
       document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
-      setIsDarkMode(false);
+      localStorage.setItem("theme", "dark");
     }
   }, []);
 
@@ -33,13 +36,13 @@ export const ThemeToggle = () => {
       onClick={toggleTheme}
       className={cn(
         "fixed max-sm:hidden top-16 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
+        "focus:outline-hidden"
       )}
     >
       {isDarkMode ? (
-        <Sun className="h-6 w-6 text-yellow-300" />
-      ) : (
         <Moon className="h-6 w-6 text-blue-900" />
+      ) : (
+        <Sun className="h-6 w-6 text-yellow-300" />
       )}
     </button>
   );
